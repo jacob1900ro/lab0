9ac51752fca5b7930d280dba4d0ae1caf95e1b7d
Jacob Rodriguez <jerd1900@gmail.com>
2024-03-27 23:29:45 -0300

    cambios

--- main.cpp
+++ main.cpp
@@ -1,8 +1,11 @@
-#include <iostream>
+
 #include <string>
 #include <vector>
 #include <set>
+#include <iostream>
+#include "DTInfoEstudiante.h"
 
+/*
 class DTInfoEstudiante
 {
 private:
@@ -17,6 +20,7 @@ public:
     std::cout << "se borro correctamente";
   };
 };
+*/
 
 std::ostream &operator<<(std::ostream &o, const DTInfoEstudiante &estudiante)
 {
@@ -32,7 +36,7 @@ private:
 public:
   DTFecha(int d, int m, int a) : dia(d), mes(m), anio(a) {} // Constructor
   ~DTFecha()
-  { // Destructorctor
+  { // Destructor
     std::cout << "se elimino corectamente";
   }
   int getDia() { return dia; };
@@ -40,6 +44,7 @@ public:
   int getAnio() { return anio; };
 };
 
+class Informacion;      //Desclaracion forward de la clase informacion
 class Estudiante
 {
 private:
@@ -71,14 +76,14 @@ void Estudiante::eliminarInfo (Informacion *i){
     m++;
   }
   if (m<vectorInfo.size()){
-    vectorInfo.erase(m);
+    vectorInfo.erase(vectorInfo.begin()+m);
   }
   else {
     std::cout<<"La cagamos xd";
   }
 }
 
-void Estudiante::InsInfo(Informacion *i)
+void Estudiante::insInfo(Informacion *i)
 {
   vectorInfo.push_back(i); // se crea un lugar en la informacion
   i->insEst(this);////////////////////falta agregarke al estudiante la informacion 
@@ -130,7 +135,7 @@ Informacion::~Informacion(){            //Destructor
 }
 
 
-void Informacion::InsEst(Estudiante *e)
+void Informacion::insEst(Estudiante *e)
 {
   vectorEst.push_back(e); // se crea un lugar en la informacion
   e->insInfo(this);   //////////////////// agregarle a la informacion el estuidante
@@ -142,7 +147,7 @@ void Informacion::eliminarEst (Estudiante * e){
     m++;
   }
   if (m<vectorInfo.size()){
-    vectorEst.erase(m);
+    vectorEst.erase(vectorEst.begin()+m);
   }
   else {
     std::cout<<"La cagamos xd ";
@@ -210,27 +215,17 @@ void imprimir(Estudiante estudiante){
     }
 };
 
-main()
+int main() 
 {
-  PaginaWeb paginaWeb1(1, DTFecha(7, 3, 2024), "https://eva.fing.edu.uy/pluginfile.php/468051/mod_resource/content/4/Guia01_P42024_IntroCBasicos.pdf", "El objetivo de esta semana es contextualizar el paradigma de Orientación a Objetos (OO) en el marco de la Ingeniería de Software, así como comenzar a ver sus conceptos básicos y cómo éstos se implementan en C++.");
+  PaginaWeb paginaWeb1(1, DTFecha(7, 3, 2024),"" ,"Programación 4 Guía Semana 1 (4/3)","https://eva.fing.edu.uy/pluginfile.php/468051/mod_resource/content/4/Guia01_P42024_IntroCBasicos.pdf", "El objetivo de esta semana es contextualizar el paradigma de Orientación a Objetos (OO) en el marco de la Ingeniería de Software, así como comenzar a ver sus conceptos básicos y cómo éstos se implementan en C++.");
 
-  PaginaWeb paginaWeb2(2, DTFecha(5, 3, 2024), "Programación orientada a objetos", "https://es.wikipedia.org/wiki/Programaci%C3%B3n_orientada_a_objetos","La programación orientada a objetos (POO, en español) es un paradigma de
-programación que parte del concepto de objetos como base, los cuales contienen
-información en forma de campos (a veces también referidos como atributos o
-propiedades) y código en forma de métodos. Algunas características clave de la
-programación orientada a objetos son herencia, cohesión, abstracción, polimorfismo,
-acoplamiento y encapsulamiento.
-");
+  PaginaWeb paginaWeb2(2, DTFecha(5, 3, 2024), "Programación orientada a objetos", "https://es.wikipedia.org/wiki/Programaci%C3%B3n_orientada_a_objetos","La programación orientada a objetos (POO, en español) es un paradigma deprogramación que parte del concepto de objetos como base, los cuales contienen información en forma de campos (a veces también referidos como atributos o propiedades) y código en forma de métodos. Algunas características clave de la programación orientada a objetos son herencia, cohesión, abstracción, polimorfismo, acoplamiento y encapsulamiento.");
 
-  chatGPT chatGPT3(3, DTFecha(8, 3, 2024), "¿Qué es el polimorfismo en orientación a objetos?",
-                   "El polimorfismo en programación orientada a objetos se refiere a la capacidad de un objeto de tomar múltiples formas. Puede ser estático, resuelto en tiempo de compilación, basado en la herencia, o dinámico, resuelto en tiempo de ejecución, asociado a interfaces o métodos abstractos. En esencia, permite que objetos de diferentes clases respondan a la misma interfaz de manera coherente, facilitando la flexibilidad y extensibilidad del código.");
+  chatGPT chatGPT3(3, DTFecha(8, 3, 2024), "¿Qué es el polimorfismo en orientación a objetos?", "El polimorfismo en programación orientada a objetos se refiere a la capacidad de un objeto de tomar múltiples formas. Puede ser estático, resuelto en tiempo de compilación, basado en la herencia, o dinámico, resuelto en tiempo de ejecución, asociado a interfaces o métodos abstractos. En esencia, permite que objetos de diferentes clases respondan a la misma interfaz de manera coherente, facilitando la flexibilidad y extensibilidad del código.");
                    
-  chatGPT chatGPT4(4, DTFecha(5,3,2024), "¿Qué es el acoplamiento en orientación a objetos?", 
-                   "El acoplamiento en programación orientada a objetos se refiere al grado de dependencia entre las clases o módulos de un sistema. Un bajo acoplamiento es deseable, ya que implica que las clases son independientes entre sí, lo que facilita la modificación, mantenimiento y reutilización del código. Por otro lado, un alto acoplamiento indica una fuerte interdependencia entre las clases, lo que puede hacer que el sistema sea más difícil de entender, modificar y mantener.");
+  chatGPT chatGPT4(4, DTFecha(5,3,2024), "¿Qué es el acoplamiento en orientación a objetos?", "El acoplamiento en programación orientada a objetos se refiere al grado de dependencia entre las clases o módulos de un sistema. Un bajo acoplamiento es deseable, ya que implica que las clases son independientes entre sí, lo que facilita la modificación, mantenimiento y reutilización del código. Por otro lado, un alto acoplamiento indica una fuerte interdependencia entre las clases, lo que puede hacer que el sistema sea más difícil de entender, modificar y mantener.");
 
-  Libro libro5(5, DTFecha(15,3,2024), "Applying UML and Patterns: An Introduction to Object-Oriented Analysis and Design and Iterative Development", 
-  "Craig Larman",
-  "Applying UML and Patterns is the world’s #1 business and college introduction to “thinking in objects”―and using that insight in real-world objectoriented analysis and design. Building on two widely acclaimed previous editions, Craig Larman has updated this book to fully reflect the new UML 2 standard, to help you master the art of object design, and to promote high-impact, iterative, and skillful agile modeling practices.");
+  Libro libro5(5, DTFecha(15,3,2024), "Applying UML and Patterns: An Introduction to Object-Oriented Analysis and Design and Iterative Development", "Craig Larman", "Applying UML and Patterns is the world’s #1 business and college introduction to “thinking in objects”―and using that insight in real-world objectoriented analysis and design. Building on two widely acclaimed previous editions, Craig Larman has updated this book to fully reflect the new UML 2 standard, to help you master the art of object design, and to promote high-impact, iterative, and skillful agile modeling practices.");
 
   std::cout << paginaWeb1.toString() << std::endl;
   std::cout << paginaWeb2.toString() << std::endl;
